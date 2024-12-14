65edfa4665cf55867e89445c1f5424357b89fe13
EliteTools <elitetoolsife@gmail.com>
2024-12-14 23:01:10 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+
+Route::get('login', [AuthController::class, 'loginForm']);  // Login form
+Route::post('login', [AuthController::class, 'login']);  // Login attempt
+Route::get('home', [HomeController::class, 'index'])->middleware('auth');  // Dashboard
+
 Route::get('/', function () {
     return view('welcome');
 });
